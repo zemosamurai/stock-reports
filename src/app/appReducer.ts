53a5c94237af3bd610940d0741102cd4ce278e7d
zemosamurai <zemosamurai@gmail.com>
2023-04-09 01:05:04 +0300

    feat: app reducer from on loading and error

---
+++ src/app/appReducer.ts
@@ -0,0 +1,23 @@
+const initialState = {
+  loading: false,
+  error: '' as string | null
+};
+
+export const appReducer = (state = initialState, action: ActionsType): InitialStateType => {
+  switch (action.type) {
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload };
+    case 'SET_ERROR':
+      return { ...state,  error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const setLoadingAC = (isLoading: boolean) => ({type: 'SET_LOADING', payload: isLoading} as const);
+export const setErrorAC = (error: string | null) => ({type: 'SET_ERROR', payload: error} as const);
+
+type InitialStateType = typeof initialState
+type ActionsType =
+  | ReturnType<typeof setLoadingAC>
+  | ReturnType<typeof setErrorAC>
