26ef3bd3b5e9eda8d2e05707c4b61a5be04e8beb
zemosamurai <zemosamurai@gmail.com>
2023-04-09 14:55:15 +0300

    feat: use rssuite lib for table

--- src/feature/stock/StockTable/StockTable.tsx
+++ src/feature/stock/StockTable/StockTable.tsx
@@ -1,60 +1,90 @@
-import {useAppDispatch, useAppSelector} from '../../../app/store';
+import {useAppSelector} from 'app/store';
 import {fetchStocksType} from '../stockAPI';
-import {page, setPageCountAC} from '../stockReducer';
+
+import 'rsuite/dist/rsuite.min.css';
+import Table from 'rsuite/Table';
 import s from './stockTable.module.css'
-import {THead} from './THead';
-import {TBodyRow} from './TBodyRow';
-import {useState} from 'react';
-import {Loading} from '../../../common/components/Loading/Loading';
+
+const {Column, HeaderCell, Cell} = Table;
 
 export const StockTable = () => {
   const stocks = useAppSelector<fetchStocksType[]>(state => state.stocks.stocksData)
-  const pageCount = useAppSelector(state => state.stocks.pageCount)
   const isLoading = useAppSelector(state => state.app.loading)
-  const dispatch = useAppDispatch()
-
   const currentStocks = stocks.slice(-10)
-  const [startIndex, setCurrentIndex] = useState(0)
-
-  const handlePrevPage = () => {
-    if (stocks.length === page.minimalSize) return
-    dispatch(setPageCountAC(pageCount - page.size))
-    setCurrentIndex(startIndex - page.size)
-  }
-  const handleNextPage = () => {
-    dispatch(setPageCountAC(pageCount + page.size))
-    setCurrentIndex(startIndex + page.size)
-  }
+  const colorChanges = (change: number) => change > 0 ? s.increase : change < 0 ? s.decline : s.previous
 
   return (
-    <div className={s.container}>
-      <table className={s.table}>
-        <THead/>
-        <tbody>
-        {isLoading && <tr className={s.loader_row}><td className={s.loader}><Loading/></td></tr>}
+      <Table
+        height={500}
+        width={1400}
+        data={currentStocks}
+        bordered
+        loading={isLoading}>
 
-        {currentStocks.map((stock: fetchStocksType, index) => (
-          <TBodyRow
-            key={stock.symbol}
-            symbol={stock.symbol}
-            open={stock.open}
-            change={stock.change}
-            close={stock.close}
-            changePercent={stock.changePercent}
-            companyName={stock.companyName}
-            marketCap={stock.marketCap}
-            latestTime={stock.latestTime}
-            latestPrice={stock.latestPrice}
-            position={index + startIndex + 1} // for current row number
-            week52High={stock.week52High}
-            week52Low={stock.week52Low}
-          />
-        ))}
-        </tbody>
-      </table>
+        <Column width={50} align='left'>
+          <HeaderCell>№</HeaderCell>
+          <Cell dataKey='id'/>
+        </Column>
 
-      <button disabled={!startIndex || isLoading} onClick={handlePrevPage}>prev</button>
-      <button disabled={isLoading} onClick={handleNextPage}>next</button>
-    </div>
+        <Column width={350} align='left'>
+          <HeaderCell>Company</HeaderCell>
+          <Cell>
+            {rowData => <span>{rowData.companyName} ({rowData.symbol})</span>}
+          </Cell>
+        </Column>
+
+        <Column width={100} align='center'>
+          <HeaderCell>Latest Price</HeaderCell>
+          <Cell dataKey='latestPrice'/>
+        </Column>
+
+        <Column width={200} align='center'>
+          <HeaderCell>Open / Close</HeaderCell>
+          <Cell>
+            {rowData => <span>{rowData.open} / {rowData.close}</span>}
+          </Cell>
+        </Column>
+
+        <Column width={125} align='center'>
+          <HeaderCell>Change</HeaderCell>
+          <Cell>
+            {rowData => (
+              <span className={colorChanges(rowData.change)}>
+                {rowData.change.toFixed(2)}
+              </span>
+            )}
+          </Cell>
+        </Column>
+
+        <Column width={125} align='center'>
+          <HeaderCell>Change %</HeaderCell>
+          <Cell>
+            {rowData => (
+              <span className={colorChanges(rowData.change)}>
+                {rowData.changePercent.toFixed(2)} %
+              </span>
+            )}
+          </Cell>
+        </Column>
+
+        <Column width={150} align='center'>
+          <HeaderCell>Low / High Year</HeaderCell>
+          <Cell>
+            {rowData => <span>{rowData.week52Low} / {rowData.week52High}</span>}
+          </Cell>
+        </Column>
+
+        <Column width={200} align='center'>
+          <HeaderCell>Market Cap</HeaderCell>
+          <Cell>
+            {rowData => <span>{rowData.marketCap.toLocaleString('ru-RU')}$</span>}
+          </Cell>
+        </Column>
+
+        <Column width={100} align='center'>
+          <HeaderCell>Latest Time</HeaderCell>
+          <Cell dataKey='latestTime'/>
+        </Column>
+      </Table>
   );
 };
